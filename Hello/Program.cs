265f5d9d4d9d5f3429e25ab62924eebdc947ad2e
Gaoxiaolong <1025941481@qq.com>
2018-05-23 20:49:29 +0800

    3

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -11,7 +11,7 @@ namespace Hello
         static void Main(string[] args)
         {
             Console.WriteLine("Hello!");
-            Console.WriteLine("you are good!")
+            Console.WriteLine("you are good!");
             Console.ReadLine();
         }
     }
